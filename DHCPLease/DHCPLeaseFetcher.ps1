642343ade6f6efa40c949ed0faf9573785cb6cbe
Zackyo <stianjhamborg@hotmail.com>
2024-12-03 15:05:22 +0100

    Oppdatert DHCPLeaseFetcher.ps1
    Første utkast av skriptet ble skrevet med feil tankegang. Måtte gjøre den om slik at skriptet lager en tom liste av scopes og deretter itererer og bruker disse scope ID'ene da det ikke fungerer å bruke "Get-DhcpServerv4Lease -ScopeId *", da wildcard ikke støttes.
    
    Samtidig sørget jeg for at skriptet skulle vise at skriptet (Write-Host) har kjørt i 3 sekunder før Powershell vinduet lukker seg.

--- DHCPLease/DHCPLeaseFetcher.ps1
+++ DHCPLease/DHCPLeaseFetcher.ps1
@@ -2,10 +2,19 @@
 $outputFile = "C:\DHCP_Leases.csv"
 
 # Henter DHCP-lease fra serveren
-$dhcpLeases = Get-DhcpServerv4Lease -ScopeId *
+$dhcpScopes = Get-DhcpServerv4Scope
+
+# Initialiser en tom liste for leieavtaler
+$allLeases = @()
+
+# Går gjennom hvert omfang og henter lease-tida
+foreach ($scope in $dhcpScopes) {
+    $leases = Get-DhcpServerv4Lease -ScopeId $scope.ScopeId
+    $allLeases += $leases
+}
 
 # Oppretter et tilpasset objekt for hver lease med de ønskede feltene
-$leaseData = $dhcpLeases | ForEach-Object {
+$leaseData = $allLeases | ForEach-Object {
     [PSCustomObject]@{
         ScopeId      = $_.ScopeId
         IPAddress    = $_.IPAddress
@@ -20,3 +29,5 @@ $leaseData | Export-Csv -Path $outputFile -Delimiter ';' -NoTypeInformation -Enc
 
 # Skriv ut melding for å bekrefte at eksporten er ferdig :)
 Write-Host "DHCP-leases har blitt eksportert til $outputFile"
+
+Sleep 3
